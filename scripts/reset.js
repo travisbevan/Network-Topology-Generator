d98be0c7e61a70091d3365a8e4d1596685ab6427
Travis Bevan <32349942+travisbevan@users.noreply.github.com>
2025-02-18 10:08:57 -0600

    Update reset.js

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,9 +1,11 @@
-document.getElementById("reset_configuration").addEventListener("click", function () {
-  document.getElementById("routers").value = 1;
-  document.getElementById("switches").value = 1;
-  document.getElementById("vlans").value = 1;
-  document.getElementById("router-ip-container").innerHTML = "";
-  document.getElementById("switch-ip-container").innerHTML = "";
-  document.getElementById("vlan-ip-container").innerHTML = "";
-  document.getElementById("network-output").style.display = "none";
-});
+function resetConfiguration() {
+  document.getElementById('routers').value = '';
+  document.getElementById('switches').value = '';
+  document.getElementById('vlans').value = '';
+  document.getElementById('router-ip-container').innerHTML = '';
+  document.getElementById('switch-ip-container').innerHTML = '';
+  document.getElementById('vlan-ip-container').innerHTML = '';
+  document.getElementById('devices-vlan-container').innerHTML = '';
+  document.getElementById('use-vlans').checked = false;
+  document.getElementById('vlan-ip-container').style.display = 'none';
+}
