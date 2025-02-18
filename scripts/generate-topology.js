cca617935a3e089151c52c42aed61d75b41ae429
Travis Bevan <32349942+travisbevan@users.noreply.github.com>
2025-02-18 10:25:45 -0600

    Update generate-topology.js

--- scripts/generate-topology.js
+++ scripts/generate-topology.js
@@ -5,14 +5,16 @@ function generateNetworkTopology() {
   const routerCount = document.getElementById('routers').value;
   config += '<div style="flex: 1; display: flex; flex-direction: column;">';
   if (routerCount > 0) {
-    config += <h4>Routers: ${routerCount}</h4>;
+    config += `<h4>Routers: ${routerCount}</h4>`;
     for (let i = 1; i <= routerCount; i++) {
-      const routerIp = document.getElementById('router' + i + '_ip').value;
-      config += <p>Router ${i}: IP - ${routerIp}</p>;
+      let routerIp = document.getElementById('router' + i + '_ip').value;
+      routerIp = routerIp.replace(/^0+/, ''); // Remove leading zeros
+      config += `<p>Router ${i}: IP - ${routerIp}</p>`;
       for (let j = 1; j <= 3; j++) {
-        const portIp = routerIp.substring(0, routerIp.lastIndexOf('.') + 1) +
-          (parseInt(routerIp.split('.').pop()) + j);
-        config += <p>Router Port ${j} IP: ${portIp}</p>;
+        // Increment IP from the base router IP, not from the last IP used
+        let portIp = routerIp.split('.');
+        portIp[3] = (parseInt(portIp[3]) + j).toString();
+        config += `<p>Router Port ${j} IP: ${portIp.join('.')}</p>`;
       }
     }
   }
@@ -22,14 +24,15 @@ function generateNetworkTopology() {
   const switchCount = document.getElementById('switches').value;
   config += '<div style="flex: 1; display: flex; flex-direction: column;">';
   if (switchCount > 0) {
-    config += <h4>Switches: ${switchCount}</h4>;
+    config += `<h4>Switches: ${switchCount}</h4>`;
     for (let i = 1; i <= switchCount; i++) {
-      const switchIp = document.getElementById('switch' + i + '_ip').value;
-      config += <p>Switch ${i}: IP - ${switchIp}</p>;
+      let switchIp = document.getElementById('switch' + i + '_ip').value;
+      switchIp = switchIp.replace(/^0+/, '');
+      config += `<p>Switch ${i}: IP - ${switchIp}</p>`;
       for (let j = 1; j <= 3; j++) {
-        const portIp = switchIp.substring(0, switchIp.lastIndexOf('.') + 1) +
-          (parseInt(switchIp.split('.').pop()) + j);
-        config += <p>Switch Port ${j} IP: ${portIp}</p>;
+        let portIp = switchIp.split('.');
+        portIp[3] = (parseInt(portIp[3]) + j).toString();
+        config += `<p>Switch Port ${j} IP: ${portIp.join('.')}</p>`;
       }
     }
   }
@@ -39,17 +42,18 @@ function generateNetworkTopology() {
   const vlanCount = document.getElementById('vlans').value;
   config += '<div style="flex: 1; display: flex; flex-direction: column;">';
   if (vlanCount > 0) {
-    config += <h4>VLANs: ${vlanCount}</h4>;
+    config += `<h4>VLANs: ${vlanCount}</h4>`;
     for (let i = 1; i <= vlanCount; i++) {
-      const vlanSubnet = document.getElementById('vlan' + i + '_subnet').value;
-      config += <p>VLAN ${i}: Subnet ${vlanSubnet}</p>;
+      let vlanSubnet = document.getElementById('vlan' + i + '_subnet').value;
+      vlanSubnet = vlanSubnet.replace(/^0+/, '');
+      config += `<p>VLAN ${i}: Subnet ${vlanSubnet}</p>`;
       const devicesInVlanInput = document.getElementById('devices_in_vlan' + i);
       if (devicesInVlanInput) {
         const devicesInVlan = devicesInVlanInput.value;
         for (let j = 1; j <= devicesInVlan; j++) {
-          const deviceIp = vlanSubnet.substring(0, vlanSubnet.lastIndexOf('.') + 1) +
-            (parseInt(vlanSubnet.split('.').pop()) + j);
-          config += <p>Device ${j} IP: ${deviceIp}</p>;
+          let deviceIp = vlanSubnet.split('/')[0].split('.');
+          deviceIp[3] = (parseInt(deviceIp[3]) + j).toString();
+          config += `<p>Device ${j} IP: ${deviceIp.join('.')}</p>`;
         }
       }
     }
